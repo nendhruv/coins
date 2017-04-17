214faa6adf82970cec5855c935ebbf5037780b36
nendhruv <nendhruv@gmail.com>
2017-04-18 00:25:05 +0530

    coin calculator

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var express = require('express'),
   app = module.exports = express();
   server = require('http').createServer(app);
 
-var trucks = require('./server/controllers.js')
-
-
 app.set('port', process.env.PORT || 3000);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -14,10 +11,6 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
   res.render('index')
 });
-
-
-app.get('/api/getfoodtrucks', trucks.findInLocation)
-
 
 server.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,81 +1,61 @@
-angular.module('FoodTruckApp.controllers', ['ngRoute', 'ui.router','ngMap', 'google.places'])
-.controller('HomeCtrl', function($scope, NgMap, $http){
-  $scope.loading = false;
-  $scope.place = '';
-  // console.log($scope.place);
-  $scope.defaultLat = 37.7749;
-  $scope.defaultLong = -122.4194;
-  $scope.truckDetails = function(truck){
-    // console.log(truck)
-    $scope.truck = truck
+angular.module('CoinCalculatorApp.controllers', [])
+.controller('HomeCtrl', function($scope, $http){
+
+  $scope.warningMessage = false;
+
+  var curency = [200,100,50,20,10,5,2,1]
+
+  $scope.calculate = function(amount){
+  	var isValidated = $scope.validate(amount)
+  	if(isValidated){
+	  	amount = $scope.isPound(amount)
+	  	amount = parseInt(amount)
+	  	$scope.coins = curency.map(function(coin, i){
+	  		var curr = Math.floor(amount/coin)
+	  		amount %= coin
+	  		return {count: curr, value: coin}
+	  	})
+	  	$scope.result = $scope.transform($scope.coins)
+	  	$scope.warningMessage = false
+  	}
+  	else{
+  		$scope.warningMessage = true;
+  	}
+
   }
 
-  $scope.truckList = function(){
-    $scope.truck = false;
+
+  $scope.transform = function(obj){
+  	for (var i = 0; i < obj.length; i++) {
+  		if(obj[i].value === 100 || obj[i].value === 200){
+  			obj[i].value = '£'+(obj[i].value/100)
+  		}
+  		else{
+  			obj[i].value += 'p'
+  		}
+  	}
+
+  	return obj;
   }
 
-  $scope.$watch('place', function(){
-    if(typeof($scope.place) === "object"){
-      $scope.userLat = $scope.place.geometry.location.lat();
-      // console.log($scope.place.geometry.location)
-      $scope.userLong = $scope.place.geometry.location.lng();
-      var kmInLongitudeDegree = 111.320 * Math.cos( $scope.userLat / 180.0 * Math.PI)
-      var deltaLong = 2 / kmInLongitudeDegree; // 2 is radius in km
-      var deltaLat = 2 / 111.1; // 2 is radius in km
 
-      var minLat = $scope.userLat - deltaLat;
-      var maxLat = $scope.userLat + deltaLat;
-      var minLong = $scope.userLong - deltaLong;
-      var maxLong = $scope.userLong + deltaLong;
-      $scope.loading = true;
-      $http.get('/api/getfoodtrucks?minlat='+minLat+'&maxlat='+maxLat+'&minlong='+minLong+'&maxlong='+maxLong+'&type=Truck').success(function(data){
-        // console.log(data)
-        if(data.err){
-          $scope.defaultLat = 37.7749;
-          $scope.defaultLong = -122.4194;
-          $scope.msg = 'Oops there is no data to display'
-          $scope.data = null
+  $scope.isPound = function(str){
+  	if(str.indexOf('£') !== -1){
+  		str = $scope.removeChar(str)
+  		str *= 100 
+  	}
 
-        }
-        else if(data.data.length === 0){
-          $scope.defaultLat = 37.7749;
-          $scope.defaultLong = -122.4194;
-          $scope.msg = 'Oops there is no data to display'
-          $scope.data = null
-        }
-        else{
-          $scope.data = data.data
-          $scope.defaultLat = $scope.userLat
-          $scope.defaultLong = $scope.userLong
-        }
-        $scope.loading = false;
-      })
-
-    }
-  })
-  // NgMap.getMap().then(function(map) {
-  //   console.log('dd');
-  //   console.log(map.getCenter());
-  //   console.log('markers', map.markers);
-  //   console.log('shapes', map.shapes);
-  // });
-  $scope.pos = function(h){
-    // console.log(h);
+  	return str;
   }
-})
-.config(['$httpProvider', function ($httpProvider) {
-		$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-}])
-.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "../partials/home.html",
-      controller: "HomeCtrl"
-  	});
+
+
+  $scope.removeChar = function(str){
+  	return str.replace(/[£p]+/g, '');
+  }
+
+  $scope.validate = function(str){
+  	var regex = /^(\u00A3)?([0-9\.]+)p?$/;
+  	return regex.test(str);
+  }
+
 });
